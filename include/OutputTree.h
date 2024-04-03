fe34257f2f4bf3ce749152f1dddd0253c935f3e5
Diego Baron <diegoa_baronm@hotmail.com>
2024-04-03 16:35:25 +0100

    Removing member variables not needed.

--- include/OutputTree.h
+++ include/OutputTree.h
@@ -23,19 +23,10 @@ class OutputTree {
     public: // To be able to access this members directly and assign in FillTree.
     double m_mcWeight;
     double m_mjj;
-    double m_deltaRapidity;
     double m_deltaPhiLT;
-    double m_deltaPhiJJ;
     double m_jetRNNScore;
-    double m_ptBalance;
-    double m_zCentrality;
-    double m_omega;
-    double m_reco_mass;
-    double m_lepNuPt;
     double m_transverseMassLep;
-    double m_transverseRecoMassVariable;
     double m_massTauLep;
-    int m_nLightJets;
     double m_tau_pT;
     double m_lep_pT;
     double m_jet0_pT;
